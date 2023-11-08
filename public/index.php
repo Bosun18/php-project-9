c6c0734c9031cb0a164a30c82a866fe0e8adcebd
Bosun <beliy-81@mail.ru>
2023-11-08 18:22:13 +0300

    updated index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php
 
+use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Middleware\MethodOverrideMiddleware;
+use Slim\Flash;
+use Carbon\Carbon;
+use Valitron\Validator;
+use Src\Database;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\TransferException;
+use DiDom\Document;
+use Illuminate\Support;
+
 // Путь который будет использован при глобальной установке пакета
 $autoloadPath1 = __DIR__ . '/../../../autoload.php';
 // Путь для локальной работы с проектом
@@ -10,3 +22,13 @@ if (file_exists($autoloadPath1)) {
 } else {
     require_once $autoloadPath2;
 }
+
+$app = AppFactory::create();
+
+// Обработчик
+$app->get('/', function ($request, $response) {
+    return $response->write('Welcome to Page Analyzer!');
+});
+
+
+$app->run();
